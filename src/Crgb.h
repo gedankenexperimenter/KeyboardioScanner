39eb12071b859dda7e34c4333d05d5b1b7b6161d
Michael Richters <gedankenexperimenter@gmail.com>
2018-01-16 14:31:40 -0600

    Constructors for class Crgb

--- src/Crgb.h
+++ src/Crgb.h
@@ -9,6 +9,9 @@ struct Crgb {
   byte b;
   byte g;
   byte r;
+
+  Crgb() = default; // I think this initializes to zeros
+  constexpr Crgb(byte r, byte g, byte b) : r(r), g(g), b(b) {}
 };
 
 } // namespace hardware {
